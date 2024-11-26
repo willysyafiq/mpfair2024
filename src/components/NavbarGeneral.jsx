1a22d2891faf614743dd3e6a148583f89e17cb50
willysyafiq <willy.syafiq@gmail.com>
2024-11-27 05:55:10 +0700

    Updated Dropdown Menu, Countdown Elements, Placeholder Pages

--- src/components/NavbarGeneral.jsx
+++ src/components/NavbarGeneral.jsx
@@ -18,12 +18,19 @@ const HamburgerMenu = () => {
           />
         </a>
         <div className="hamburger cursor-pointer z-50" onClick={toggleMenu}>
-          <img src="/hamburger-menu.svg" alt="Hamburger Menu" className="h-8" />
+          <img
+            src={isOpen ? "/close-menu.svg" : "/hamburger-menu.svg"}
+            alt="Menu Icon"
+            className="h-8"
+          />
         </div>
       </div>
-
+      {/* Dropdown Menu Container */}
       <div
-        className={`menu fixed top-0 left-0 w-full h-full bg-black bg-opacity-90 text-white flex flex-col justify-center items-center z-40 ${isOpen ? "flex" : "hidden"}`}
+        className={`menu fixed top-0 left-0 w-full h-full bg-cover bg-white bg-center text-black flex flex-col justify-center items-center z-40 ${
+          isOpen ? "flex" : "hidden"
+        }`}
+        style={{ backgroundImage: "url('/background-b.svg')" }}
       >
         <div
           className="close cursor-pointer absolute top-5 right-5"
@@ -31,25 +38,61 @@ const HamburgerMenu = () => {
         >
           &times; {/* Close icon */}
         </div>
-        <nav>
-          <ul className="space-y-4 text-lg font-cabin">
-            <li>
-              <a href="#home" className="hover:underline">
-                Home
+        <nav className="w-full px-8">
+          <ul className="space-y-4 text-lg font-cabin w-full">
+            <li className="w-full">
+              <a
+                href="/tiket"
+                className="block w-full py-3 bg-[#EB1B31] text-center text-white rounded-full no-underline font-raleway font-extrabold text-[1.125rem] leading-[130%] shadow-[0px_1px_2px_0px_rgba(0,0,0,0.30),0px_1px_3px_1px_rgba(0,0,0,0.15)]"
+              >
+                Beli Tiket
               </a>
             </li>
-            <li>
-              <a href="#about" className="hover:underline">
-                About
+            <li className="w-full">
+              <a
+                href="/tiket"
+                className="block w-full py-3 bg-white text-center text-black rounded-full no-underline font-raleway font-bold text-[1.125rem] leading-[130%] shadow-[0px_1px_2px_0px_rgba(0,0,0,0.30),0px_1px_3px_1px_rgba(0,0,0,0.15)]"
+              >
+                Point Frenzy
               </a>
             </li>
-            <li>
-              <a href="#services" className="hover:underline">
-                Services
+            <li className="w-full">
+              <a
+                href="/festival"
+                className="block w-full py-3 bg-white text-center text-black rounded-full no-underline font-raleway font-bold text-[1.125rem] leading-[130%] shadow-[0px_1px_2px_0px_rgba(0,0,0,0.30),0px_1px_3px_1px_rgba(0,0,0,0.15)]"
+              >
+                Festival
               </a>
             </li>
-            <li>
-              <a href="#contact" className="hover:underline">
+            <li className="w-full">
+              <a
+                href="/contact"
+                className="block w-full py-3 bg-white text-center text-black rounded-full no-underline font-raleway font-bold text-[1.125rem] leading-[130%] shadow-[0px_1px_2px_0px_rgba(0,0,0,0.30),0px_1px_3px_1px_rgba(0,0,0,0.15)]"
+              >
+                Contact
+              </a>
+            </li>
+            <li className="w-full">
+              <a
+                href="/venue-map"
+                className="block w-full py-3 bg-white text-center text-black rounded-full no-underline font-raleway font-bold text-[1.125rem] leading-[130%] shadow-[0px_1px_2px_0px_rgba(0,0,0,0.30),0px_1px_3px_1px_rgba(0,0,0,0.15)]"
+              >
+                Venue Map
+              </a>
+            </li>
+            <li className="w-full">
+              <a
+                href="/faq"
+                className="block w-full py-3 bg-white text-center text-black rounded-full no-underline font-raleway font-bold text-[1.125rem] leading-[130%] shadow-[0px_1px_2px_0px_rgba(0,0,0,0.30),0px_1px_3px_1px_rgba(0,0,0,0.15)]"
+              >
+                FAQ
+              </a>
+            </li>
+            <li className="w-full">
+              <a
+                href="/contact"
+                className="block w-full py-3 bg-white text-center text-black rounded-full no-underline font-raleway font-bold text-[1.125rem] leading-[130%] shadow-[0px_1px_2px_0px_rgba(0,0,0,0.30),0px_1px_3px_1px_rgba(0,0,0,0.15)]"
+              >
                 Contact
               </a>
             </li>
